4a45ec52e9a8f566d6a640c27c138c3ca9f05634
Zab <74011524+Zabster98@users.noreply.github.com>
2022-07-29 10:46:44 -0500

    Update DbEncrypt.tf

--- modules/azureSQL/DbEncrypt.tf
+++ modules/azureSQL/DbEncrypt.tf
@@ -4,18 +4,9 @@ provider "azurerm"{
 
 }
 
-resource "azurerm_mssql_server" "encrypt-terratest" {
-  name                         = "encrypt-terratest-2"
-  resource_group_name          = "Zab-Policies-Test"
-  location                     = "East US"
-  version                      = "12.0"
-  administrator_login          = "4dm1n157r470r"
-  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
-}
-
 resource "azurerm_mssql_database" "test" {
   name           = "encrypt_test_2"
-  server_id      = azurerm_mssql_server.encrypt-terratest.id
+  server_id      = "/subscriptions/55f702f9-17ee-4d42-8da3-3f0bc97c4158/resourceGroups/Zab-Policies-Test/providers/Microsoft.Sql/servers/encrypt-terratest-2"
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "LicenseIncluded"
   sku_name       = "DW100c"
