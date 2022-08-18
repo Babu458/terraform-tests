fa6bd5933de31aae4b77ade0285a2bd1374fe122
Zab <74011524+Zabster98@users.noreply.github.com>
2022-08-18 10:43:18 -0500

    Update dbEncryptionEnabled.tf

--- modules/azureSQL/dbEncryptionEnabled.tf
+++ modules/azureSQL/dbEncryptionEnabled.tf
@@ -1,4 +1,4 @@
-provider "azurerm"{
+provider "azurerm" {
   features {}
   subscription_id = "55f702f9-17ee-4d42-8da3-3f0bc97c4158"
 }
@@ -11,10 +11,10 @@ terraform {
 
 #Creating an Azure SQL database
 resource "azurerm_mssql_database" "test-one" {
-  name           = var.db_name
-  server_id      = var.server_id
-  collation      = "SQL_Latin1_General_CP1_CI_AS"
-  license_type   = "LicenseIncluded"
-  sku_name       = "DW100c"
+  name                                = var.db_name
+  server_id                           = var.server_id
+  collation                           = "SQL_Latin1_General_CP1_CI_AS"
+  license_type                        = "LicenseIncluded"
+  sku_name                            = "DW100c"
   transparent_data_encryption_enabled = var.db_encryption
 }
